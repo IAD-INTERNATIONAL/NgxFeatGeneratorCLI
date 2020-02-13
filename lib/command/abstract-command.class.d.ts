56b31114d8820955aa2639d1a26275c47c3fffcc
Thibaud Dervily <thibaud.dervily@iadinternational.com>
2020-02-13 22:15:35 +0100

    fix(isolated-test): fix wrong path for isolated spec tpl

--- lib/command/abstract-command.class.d.ts
+++
@@ -1,3 +0,0 @@
-export declare abstract class AbstractCommand {
-    abstract execute(): void;
-}
--- src/commands/isolated-spec-command.class.ts
+++ src/commands/isolated-spec-command.class.ts
@@ -13,7 +13,8 @@ export class IsolatedSpecCommand extends AbstractCommand {
    * Method which execute the isolated spec generation command.
    */
   execute(): void {
-    let specTpl = fs.readFileSync(`./templates/isolated-spec.template.txt`).toString();
+    const specTplPath = `${__dirname}/../templates/isolated-spec.template.txt`;
+    let specTpl = fs.readFileSync(specTplPath).toString();
     specTpl = specTpl.replace(/%component%/g, this.commandUtil.getCamelCaseComponentName());
     specTpl = specTpl.replace(/%component-path%/g, `${this.commandUtil.getKebabComponentName()}.${this.config.treeItem.type}`);
     const specPath = `./${this.config.projectPath}/src/app/${this.commandUtil.getPrefixPath()}/${this.commandUtil.getKebabComponentName()}.${
